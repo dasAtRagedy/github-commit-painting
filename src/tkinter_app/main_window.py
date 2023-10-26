7b8467ed43d82c0003b77f63bd4b098db5dc5cbf
dasAtRagedy <66724994+dasAtRagedy@users.noreply.github.com>
2023-10-26 14:23:55 +0300

    added rounded rectangles to canvas

--- src/tkinter_app/main_window.py
+++ src/tkinter_app/main_window.py
@@ -5,7 +5,7 @@ class CommitBox:
         self.canvas = canvas
         self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2
         self.color = initial_color
-        self.rectangle = canvas.create_rectangle(x1, y1, x2, y2, fill=initial_color)
+        self.rectangle = canvas.create_round_rectangle(x1, y1, x2, y2, radius=(int(abs(y2-y1)/2)), fill=initial_color, outline="black")
         canvas.tag_bind(self.rectangle, "<Button-1>", self.left_click)
         # Different right click buttons for different systems
         canvas.tag_bind(self.rectangle, "<Button-2>", self.right_click)
@@ -23,12 +23,6 @@ class CommitBox:
         self.canvas.itemconfig(self.rectangle, fill=self.color)
     
 
-def on_click(event):
-    item = canvas.find_closest(event.x, event.y)
-    current_color = canvas.itemcget(item, "fill")
-    new_color = "red" if current_color != "red" else "green"
-    canvas.itemconfig(item, fill=new_color)
-
 def start_app():
     global root
     root = tk.Tk()
@@ -37,11 +31,14 @@ def start_app():
 
     global canvas
     box_size = 10
-    box_margin = int(box_size*0.2)
+    box_margin = 2
     canvas_height = 7*box_size + (7+1)*box_margin
     canvas_width = 53*box_size + (53+1)*box_margin
+
     canvas = tk.Canvas(root, bg="white", height=canvas_height, width=canvas_width)
     canvas.pack()
+
+    canvas.create_round_rectangle = lambda x1, y1, x2, y2, radius, **kwargs: round_rectangle(canvas, x1, y1, x2, y2, radius, **kwargs)
 
     draw_canvas(box_size, box_margin)
 
@@ -56,3 +53,27 @@ def draw_canvas(box_size = 10, box_margin = 2):
             box = CommitBox(canvas, box_margin*(1+i)+i*box_size, box_margin*(1+j)+j*box_size, box_margin*(1+i)+i*box_size+box_size, box_margin*(1+j)+j*box_size+box_size)
             row.append(box)
         boxes.append(row)
+
+def round_rectangle(canvas, x1, y1, x2, y2, radius=25, **kwargs):
+    points = [x1+radius, y1,
+              x1+radius, y1,
+              x2-radius, y1,
+              x2-radius, y1,
+              x2, y1,
+              x2, y1+radius,
+              x2, y1+radius,
+              x2, y2-radius,
+              x2, y2-radius,
+              x2, y2,
+              x2-radius, y2,
+              x2-radius, y2,
+              x1+radius, y2,
+              x1+radius, y2,
+              x1, y2,
+              x1, y2-radius,
+              x1, y2-radius,
+              x1, y1+radius,
+              x1, y1+radius,
+              x1, y1]
+
+    return canvas.create_polygon(points, **kwargs, smooth=True)
